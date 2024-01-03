a9ae581e514d8b30f69c5e6a12bc2f53f66c9d60
yebin <yebind3308@gmail.com>
2024-01-03 16:39:45 +0900

    feat: post 구현

--- controller/user/userRegisterController.js
+++ controller/user/userRegisterController.js
@@ -5,31 +5,32 @@ const userRegisterController = async (req, res) => {
 
   // 나중에 bcrypt 이용해서 암호화 하겠음!! 일단 암호화없이 진행!
 
-  console.log("====>", userName);
-  console.log("====>", userId);
-  console.log("====>", birthDay);
-  console.log("====>", email);
-  console.log("====>", password);
+  console.log("userName ====>", userName);
+  console.log("userId ====>", userId);
+  console.log("birthDay ====>", birthDay);
+  console.log("email ====>", email);
+  console.log("password ====>", password);
 
-  // await UserModel.create({
-  //   userName: userName,
-  //   userId: userId,
-  //   birthDay: birthDay,
-  //   email: email,
-  //   password: password
-  // })
-  //   .then((user) => {
-  //     console.log("user", user);
+  await UserModel.create({
+    userName: userName,
+    userId: userId,
+    birthDay: birthDay,
+    email: email,
+    password: password,
+    phoneNumber: req.body.phoneNumber,
+  });
+  // .then((user) => {
+  //   console.log("user", user);
 
-  //     if (!user.length) return res.status(404).send({ err: "User not found" });
-  //     res.setHeader("Access-Control-Allow-origin", "*");
-  //     res.setHeader("Access-Control-Allow-Credentials", "true");
+  // if (!user.length) return res.status(404).send({ err: "User not found" });
+  res.setHeader("Access-Control-Allow-origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   //     res.status(200).send(`find successfully: ${user}`);
   //   })
   //   .catch((err) => res.status(500).send(err));
 
-  // res.send("@@ 유저 페이지");
+  res.status(200).send(req.body);
 };
 
 module.exports = userRegisterController;
