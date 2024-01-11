a67c971bd9fc50d0288af83ab686c1080fc5ee78
yebin <yebind3308@gmail.com>
2024-01-11 18:23:53 +0900

    feat: hotel List DB 연결

--- controller/hotel/hotelController.js
+++ controller/hotel/hotelController.js
@@ -3,33 +3,27 @@ const ListModel = require("../../models/ListModel");
 const { connectHotelDb } = require("../../routes/dbSwitcher");
 
 const hotelController = async (req, res) => {
-  // await HotelModel.find();
-  // res.setHeader("Access-Control-Allow-origin", "*");
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // const { hotelDb } = connectHotelDb();
-  // const a = hotelDb.collection("listingsAndReviews");
-  // const b = a.findOne({ _id: "10006546" });
-  // res.status(200).json(b);
-
-  await HotelModel.find()
-    // await ListModel.find()
+  const { hotelDb } = connectHotelDb();
+  await ListModel.find()
     .then((data) => {
       res.setHeader("Access-Control-Allow-origin", "*");
       res.setHeader("Access-Control-Allow-Credentials", "true");
 
-      console.log("req ===>", req);
-
-      const { hotelDb } = connectHotelDb();
-
-      const a = hotelDb.collection("listingsAndReviews");
-      const b = a.findOne({ _id: "10006546" });
-
-      console.log("@@@ hotelDb ===> ", hotelDb);
-      console.log("@@@ a ===> ", a);
-      // console.log("@@@ b ===> ", b);
-
-      res.status(200).json(`find successfully: ${data}`);
+      const a = hotelDb
+        .collection("listingsAndReviews")
+        .findOne({ _id })
+        .limit(10)
+        .then((result) => {
+          res.status(200).json({
+            resultCode: 1000,
+            data: result,
+          });
+        })
+        .catch((err) => {
+          console.log("============================================>", err);
+        });
     })
     .catch((err) => res.status(500).send(err));
 };
+
 module.exports = hotelController;
