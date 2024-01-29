75bfc26593c1967d66ba7804de0690c7655cb40d
yebin <yebind3308@gmail.com>
2024-01-29 16:10:44 +0900

    feat: ssl, https Add

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const mongoUri = process.env.MONGO_URI;
 const Port = process.env.PORT;
 
 const https = require("https");
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync("./config/cert.key"),
+  cert: fs.readFileSync("./config/cert.crt"),
+};
+
 const cors = require("cors");
 const express = require("express");
-const app = express();
 const cookieParser = require("cookie-parser");
 const { urlencoded, json } = require("express");
 const { connect } = require("mongoose");
 const router = require("./routes/route.js");
 
+const app = express();
 app.use(cookieParser(process.env.COOKIE_SELECT));
 app.use(urlencoded({ extended: true }));
 app.use(json());
@@ -26,9 +32,17 @@ connect(mongoUri, {
   .then(() => console.log("Successfully connected to mongodb"))
   .catch((e) => console.error(e));
 
-const server = https.createServer(app);
+https.createServer(options, app).listen(Port, () => {
+  console.log(`Server listening on port ${Port}`);
+});
 
-server.listen(Port, () => console.log(`Server listening on port ${Port}`));
+// app.listen(Port, () => {
+//   console.log(`Server listening on port ${Port}`);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send(`Server listening on port ${Port}`);
+// });
 
 app.get("/", (req, res) => {
   res.send(`Server listening on port ${Port}`);
