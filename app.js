b77a0081eed7497adad5a3dff77ddb1e6d039413
yebin <yebind3308@gmail.com>
2024-01-29 17:31:16 +0900

    fix: 코드 정리 및 수정

--- app.js
+++ app.js
@@ -9,15 +9,14 @@ const options = {
   key: fs.readFileSync("./config/cert.key"),
   cert: fs.readFileSync("./config/cert.crt"),
 };
-
 const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const { urlencoded, json } = require("express");
 const { connect } = require("mongoose");
 const router = require("./routes/route.js");
-
 const app = express();
+
 app.use(cookieParser(process.env.COOKIE_SELECT));
 app.use(urlencoded({ extended: true }));
 app.use(json());
@@ -35,14 +34,6 @@ connect(mongoUri, {
 https.createServer(options, app).listen(Port, () => {
   console.log(`Server listening on port ${Port}`);
 });
-
-// app.listen(Port, () => {
-//   console.log(`Server listening on port ${Port}`);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send(`Server listening on port ${Port}`);
-// });
 
 app.get("/", (req, res) => {
   res.send(`Server listening on port ${Port}`);
