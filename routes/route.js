f6728833bc70fe8a7a36e426b7e0735159c892e4
yebin <yebind3308@gmail.com>
2024-02-01 13:42:34 +0900

    fix: 코드 정리 및 제거

--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("../controller/user/userController.js");
 const userRegisterController = require("../controller/user/userRegisterController.js");
 const loginController = require("../controller/user/loginController.js");
 const LikeController = require("../controller/like/likeController.js");
 const listController = require("../controller/hotel/listController.js");
 const authMiddleware = require("../middleware/authMiddleware.js");
 
-router.get("/user", authMiddleware, userController);
-router.post("/user", authMiddleware, userRegisterController);
+router.post("/user", userRegisterController);
 router.get("/hotelList", listController);
 router.post("/login", loginController);
 router.get("/likes", authMiddleware, LikeController);
